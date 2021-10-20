061bac0a874cb1f98f4cd9c7242288bbd9453ce6
maciek zelek <mzelechowski79@gmail.com>
2021-10-20 12:24:34 +0200

    kodowanie

--- src/pl/mzelechowski/BankAccount3.java
+++ src/pl/mzelechowski/BankAccount3.java
@@ -1,4 +1,33 @@
 package pl.mzelechowski;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class BankAccount3 {
+    private int balance;
+    private double interestRate;
+
+    public BankAccount3(int balance, double interestRate) {
+        this.balance = balance;
+        this.interestRate = interestRate;
+    }
+
+    public ActionListener createBalanceCounter(){
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int newBalance = (int) (BankAccount3.this.balance + balance * interestRate);
+                System.out.println("Changing account balance from: "+ balance + " to " + newBalance);
+                BankAccount3.this.balance = newBalance;
+            }
+        };
+    }
+
+//    public ActionListener createBalanceCounter(){
+//        return (e) -> {
+//                int newBalance = (int) (BankAccount3.this.balance + balance * interestRate);
+//                System.out.println("Changing account balance from: "+ balance + " to " + newBalance);
+//                BankAccount3.this.balance = newBalance;
+//        };
+//    }
 }
--- src/pl/mzelechowski/Main.java
+++ src/pl/mzelechowski/Main.java
@@ -1,8 +1,25 @@
 package pl.mzelechowski;
 
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+    private static void task1(){
+        initWindow();
+        BankAccount1 account = new BankAccount1(1000);
+        BankAccount1.BalanceCounter counter = account.new BalanceCounter(0.02);
+        Timer timer = new Timer(1000, counter);
+        timer.setDelay(0);
+        timer.start();
+    }
+    private static void initWindow(){
+        JFrame frame = new JFrame("Bank account");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         JButton button = new JButton("close");
     }
 }
